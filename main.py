4d2afd17cb07275a20b7380b97a492f899fc856d
Timemaster111 <53912018+Timemaster111@users.noreply.github.com>
2021-05-15 20:32:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import datetime
 from appJar import gui
 import math
 from typing import *
+import numpy as np
 
 import pyttsx3
 engine = pyttsx3.init()
@@ -24,9 +25,11 @@ def say(text):
 class GUI:
     def __init__(self):
 
-        self.olaps = 0
-        self.blaps = 0
+        self.olaps = 1
+        self.blaps = 1
         self.api = API()
+        self.ogates = 0
+        self.bgates = 0
 
         config = open("config.txt", "r")
         self.IP = config.read().splitlines()[0]
@@ -40,18 +43,18 @@ class GUI:
         app.setSticky("news")
         app.setExpand("vertical")
         # app.setPadding([2, 2])
-        app.addEntry("IP Addr", 0)
+        app.addEntry("IP Addr", 0, colspan=2)
         app.setEntryWidth("IP Addr", 60)
         app.setEntry("IP Addr", "IP: " + self.IP, callFunction=False)
         app.setEntryChangeFunction("IP Addr", self.writeIP)
         app.setEntryAlign("IP Addr", "center")
-        app.addButton("help", self.helpFunc)
+        app.addButton("help", self.helpFunc, colspan=2)
         app.setButton("help", "Click me for help!")
-        app.addLabel("status", "Waiting to connect")
-        app.addHorizontalSeparator()
-        app.addLabel("clock", "00:00:00")
-        app.addHorizontalSeparator()
-        app.addLabel("start", "Lap times").config(font="Arial 20 underline bold")
+        app.addLabel("status", "Waiting to connect", colspan=2)
+        app.addHorizontalSeparator(colspan=2)
+        app.addLabel("clock", "00:00:00", colspan=2)
+        app.addHorizontalSeparator(colspan=2)
+        app.addLabel("start", "Lap times", colspan=2).config(font="Arial 20 underline bold")
         self.timer = app.after(1000, self.checkStart)
         app.bindKey("s", self.stopClock)
         app.bindKey("r", self.resetClock)
@@ -67,24 +70,46 @@ class GUI:
         app.stopSubWindow()
 
         app.after(100, self.mainclock)
-        for i in range(5):
-            app.addLabel("B" + str(i), "BLap"+str(i), row=i+7)
-            app.setLabelTooltip("test" + str(i), "gate 1: 00:00:00, gate 1: 00:00:00, gate 1: 00:00:00")
-        for i in range(5):
-            app.addLabel("O" + str(i), "OLap" + str(i), row=i + 7, column=2)
-            app.setLabelTooltip("test" + str(i), "gate 1: 00:00:00, gate 1: 00:00:00, gate 1: 00:00:00")
+        for i in range(1, 6):
+            app.addLabel("B" + str(i), "BLap"+str(i), row=i+7, column=0, colspan=1)
+            app.setLabelTooltip("B" + str(i), "Gates: ")
+            app.setLabelWidth("B"+str(i), 20)
+        for i in range(1, 6):
+            app.addLabel("O" + str(i), "OLap" + str(i), row=i + 7, column=1, colspan=1)
+            app.setLabelTooltip("O" + str(i), "Gates: ")
 
         app.go()
 
     def mainclock(self):
         teamLapped = self.api.checkGates()
         if teamLapped:
-            if teamLapped == "B":
-                self.app.setLabel("B"+str(self.blaps), self.clock-self.start)
-                self.blaps+=1
-            if teamLapped == "O":
-                self.app.setLabel("O"+str(self.blaps), self.clock-self.start)
-                self.blaps+=1
+            if "B" in teamLapped:
+                self.app.setLabel("B"+str(self.blaps), "BLap"+str(self.blaps)+str(self.clock-self.start))
+                self.blaps += 1
+            if "O" in teamLapped:
+                self.app.setLabel("O"+str(self.blaps), "OLap"+str(self.blaps)+str(self.clock-self.start))
+                self.blaps += 1
+            if "b" in teamLapped:
+                self.bgates += 1
+                self.bgates %= 6
+                gates: str = self.app.getLabelTooltip("B"+str(self.blaps))
+                gates: str = gates[7:]
+                gates: List = gates.split(" ")
+                lastGate = gates[-1].split(": ")
+                if not lastGate[-1]:
+                    lastGate[-1] = [self.start]
+                gates.append("Gate "+str(self.bgates+1)+": "+str(self.clock-lastGate))
+            if "o" in teamLapped:
+                self.ogates += 1
+                self.ogates %= 6
+                gates: str = self.app.getLabelTooltip("O" + str(self.olaps))
+                gates: str = gates[7:]
+                gates: List = gates.split(" ")
+                lastGate = gates[-1].split(": ")
+                if not lastGate[-1]:
+                    lastGate[-1] = [self.start]
+                gates.append("Gate " + str(self.ogates+1) + ": " + str(self.clock - lastGate))
+
         self.checkStart()
         self.app.after(10, self.mainclock)
 
@@ -133,11 +158,11 @@ class GUI:
         if type(start) == Dict:
             self.start = datetime.datetime.now()
             self.clock = datetime.datetime.now()
+            self.api = API()
             self.timer = self.app.after(1, self.clockRun)
             return
         if start[1] == "r":
             self.app.setLabel("status", "ready")
-
 
 
 class API:
@@ -145,19 +170,19 @@ class API:
         config = open("config.txt", "r")
         self.IP = config.read().splitlines()[0]
         config.close()
-        self.orangeGates = [((15, 11), (2, -2), (4.1, 4)),
-                            ((15, 11), (2, -2), (-4, -4.1)),
-                            ((1, -1), (1, -1), (-35, -35.1)),
-                            ((-11, -15), (2, -2), (-4, -4.1)),
-                            ((-11, -15), (2, -2), (4.1, 4)),
-                            ((1, -1), (1, -1), (35.1, 35))]
+        self.orangeGates = [((15, 11), (2, -2), 4),
+                            ((15, 11), (2, -2), -4),
+                            ((1, -1), (1, -1), -35),
+                            ((-11, -15), (2, -2), -4),
+                            ((-11, -15), (2, -2), 4),
+                            ((1, -1), (1, -1), 35)]
         self.blueGates = [#((-11, -15), (2, -2), (5, 3)),
-            ((-11, -15), (2, -2), (-4, -4.1)),
-                          ((-11, -15), (2, -2), (4.1, 4)),
-                          ((1, -1), (1, -1), (35.1, 35)),
-                          ((15, 11), (2, -2), (4.1, 4)),
-                          ((15, 11), (2, -2), (-4, -4.1)),
-                          ((1, -1), (1, -1), (-35, -35.1))]
+            ((-11, -15), (2, -2), -4),
+                          ((-11, -15), (2, -2), 4),
+                          ((1, -1), (1, -1), 35),
+                          ((15, 11), (2, -2), 4),
+                          ((15, 11), (2, -2), -4),
+                          ((1, -1), (1, -1), -35)]
         self.counterOrange = 0
         self.counterBlue = 0
         self.orangeLaps = 0
@@ -184,12 +209,52 @@ class API:
     def averagePosition(self, a, b):
         return (a[0]+b[0])//2, (a[1]+b[1])//2, (a[2]+b[2])//2
 
+    def checkRay(self, lastPos, currentPos, gate):
+        """
+        Bunch of complicated ass maths I don't entirely understand ngl
+        :param lastPos:
+        :param currentPos:
+        :param gate:
+        :return: bool
+        """
+        planeNormal = np.array([0, 1, 0])
+        planePoint = np.array([gate[0][0], gate[1, 0], gate[2]])  # Any point on the plane
+
+        # Define ray
+        rayDirection = np.array([currentPos[0]-lastPos[0], currentPos[1]-lastPos[1], currentPos[2]-lastPos[2]])
+        rayPoint = np.array(currentPos)  # Any point along the ray
+
+        Psi = self.LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint)
+        print("intersection at", Psi)
+
+    def LinePlaneCollision(self, planeNormal, planePoint, rayDirection, rayPoint, epsilon=1e-6):
+        """
+        yes I stole this code. It comes from
+        https://rosettacode.org/wiki/Find_the_intersection_of_a_line_with_a_plane#Python
+        as of 15/05/2021
+        :param planeNormal:
+        :param planePoint:
+        :param rayDirection:
+        :param rayPoint:
+        :param epsilon:
+        :return:
+        """
+        ndotu = planeNormal.dot(rayDirection)
+        if abs(ndotu) < epsilon:
+            raise RuntimeError("no intersection or line is within plane")
+
+        w = rayPoint - planePoint
+        si = -planeNormal.dot(w) / ndotu
+        Psi = w + si * rayDirection + planePoint
+        return Psi
+
     def checkGates(self, res=None):
         #print(self.counterBlue)
         if not res:
             res = self.get()
         if not res:
             return
+        returnStr = ""
         blueGate = self.blueGates[self.counterBlue]
         orangeGate = self.orangeGates[self.counterOrange]
         #print(res["teams"][0])
@@ -197,27 +262,26 @@ class API:
         orangePlayers = res["teams"][3]["players"]
         bluePosAverage = self.averagePosition(bluePlayers[0]["head"]["position"], bluePlayers[1]["head"]["position"])
         orangePosAverage = self.averagePosition(orangePlayers[0]["head"]["position"], orangePlayers[1]["head"]["position"])
-        if blueGate[0][0] >= bluePosAverage[0] >= blueGate[0][1] and\
-           blueGate[1][0] >= bluePosAverage[1] >= blueGate[1][1] and\
-           blueGate[2][0] >= bluePosAverage[2] >= blueGate[2][1]:
+        if self.checkRay(self.bluePositions[-1], bluePosAverage, self.blueGates[self.counterBlue]):
+            returnStr += "b"
             self.counterBlue = (self.counterBlue + 1) % len(self.blueGates)
             #say("blue passed through gate"+str(self.counterBlue))
             if self.counterBlue == 0:
                 self.orangeLaps += 1
                 say("Blue Lap complete")
-                return "B"
-        if orangeGate[0][0] >= orangePosAverage[0] >= orangeGate[0][1] and \
-           orangeGate[1][0] >= orangePosAverage[1] >= orangeGate[1][1] and \
-           orangeGate[2][0] >= orangePosAverage[2] >= orangeGate[2][1]:
+                returnStr += "B"
+        if self.checkRay(self.orangePositions[-1], orangePosAverage, self.orangeGates[self.counterOrange]):
             self.counterOrange = (self.counterOrange + 1) % len(self.orangeGates)
+            returnStr += "o"
             #say("orange passed through gate"+str(self.counterOrange))
             if self.counterOrange == 0:
                 self.orangeLaps += 1
                 say("orange lap complete")
-                return "O"
+                returnStr += "O"
         self.orangePositions.append(orangePosAverage)
         self.bluePositions.append(bluePosAverage)
         #print(bluePosAverage)
+        return returnStr
 
     def checkStart(self, res=None):
         if not res:
@@ -228,16 +292,19 @@ class API:
             #print(res)
             teams = res["teams"]
             #print(teams)
-            blue = teams[1]["players"]
-            orange = []
+            blue = teams[0]["players"]
+            orange = teams[1]
+            start = False
             if len(teams) > 3:
-                orange = teams[3]["players"]
+                orange = teams[3]
+            orange = orange["players"]
             if len(blue) < 2 or len(orange) < 2:
                 return False, "p"
-            if res["game_status"] != "playing":
-                return False, "r"
             if res["game_clock"] < 0.1:
-                return True
+                start = True
+            if res["game_status"] != "playing":
+                return start, "r"
+
             return False, "e"
         except KeyError:
             return False, "l"
