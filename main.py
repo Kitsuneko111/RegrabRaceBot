2a94b2abd917ff917fd6eebca12b8288130df677
u2191273 <philip.thompson.1@warwick.ac.uk>
2021-10-12 23:49:14 +0100

    .

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ class GUI:
 
         self.app = gui("VRPL Regrab race timekeeper", stretch="both")
         app = self.app
+        app.setIcon("kisspng-stopwatch-timer-clip-art-time-5ac30c04a83206.8478606515227320366889.jpg")
         app.setSticky("news")
         app.setExpand("vertical")
-        # app.setPadding([2, 2])
         app.addEntry("IP Addr", 0, colspan=2)
         app.setEntryWidth("IP Addr", 60)
         app.setEntry("IP Addr", "IP: " + self.IP, callFunction=False)
@@ -68,31 +68,24 @@ class GUI:
         teamLapped = self.api.checkGates()
         if teamLapped:
             if "B" in teamLapped:
-                self.app.setLabel("B"+str(self.blaps), "BLap"+str(self.blaps)+str(self.clock-self.start))
+                self.app.setLabel("B"+str(self.blaps), "BLap "+str(self.blaps)+" "+str(self.clock-self.start))
                 self.blaps += 1
             if "O" in teamLapped:
-                self.app.setLabel("O"+str(self.blaps), "OLap"+str(self.blaps)+str(self.clock-self.start))
+                self.app.setLabel("O"+str(self.blaps), "OLap "+str(self.blaps)+" "+str(self.clock-self.start))
                 self.blaps += 1
             if "b" in teamLapped:
                 self.bgates += 1
                 self.bgates %= 6
-                gates: str = self.app.getLabelTooltip("B"+str(self.blaps))
-                gates: str = gates[7:]
-                gates: List = gates.split(" ")
-                lastGate = gates[-1].split(": ")
-                if not lastGate[-1]:
-                    lastGate[-1] = [self.start]
-                gates.append("Gate "+str(self.bgates+1)+": "+str(self.clock-lastGate))
+                #gates: str = self.app.getLabelTooltip("BLap"+str(self.blaps))
+                #gates: str = gates[7:]
+                #gates: List = gates.split(" ")
+                #lastGate = gates[-1].split(": ")
+               # if not lastGate[-1]:
+                #    lastGate[-1] = [self.start]
+                #gates.append("Gate "+str(self.bgates+1)+": "+str(self.clock-lastGate))
             if "o" in teamLapped:
                 self.ogates += 1
                 self.ogates %= 6
-                gates: str = self.app.getLabelTooltip("O" + str(self.olaps))
-                gates: str = gates[7:]
-                gates: List = gates.split(" ")
-                lastGate = gates[-1].split(": ")
-                if not lastGate[-1]:
-                    lastGate[-1] = [self.start]
-                gates.append("Gate " + str(self.ogates+1) + ": " + str(self.clock - lastGate))
 
         self.checkStart()
         self.app.after(10, self.mainclock)
@@ -100,9 +93,7 @@ class GUI:
     def writeIP(self):
         config = open("config.txt", "r")
         configVals = config.read().splitlines(keepends=True)
-        #print(configVals)
         configVals = configVals[1:]
-        #print(configVals)
         config.close()
         config = open("config.txt", "w")
         config.write(self.app.getEntry("IP Addr")[4:]+"\n"+"".join(configVals))
@@ -117,7 +108,6 @@ class GUI:
         diff = diff.replace(".", ":")
         diff = diff.split(":")
         diff = diff[1:]
-        # print(diff)
         if len(diff) > 2:
             diff[2] = diff[2][:2]
         self.app.setLabel("clock", ":".join(diff))
@@ -133,13 +123,11 @@ class GUI:
     def resetClock(self):
         self.start = datetime.datetime.now()
         self.clock = datetime.datetime.now()
-        # print(self.clock-self.start)
         self.stopClock()
 
     def checkStart(self):
-        #print(".")
         start = self.api.checkStart()
-        if type(start) == Dict:
+        if start[0]:
             self.start = datetime.datetime.now()
             self.clock = datetime.datetime.now()
             self.api = API()
@@ -154,18 +142,13 @@ class API:
         config = open("config.txt", "r")
         self.IP = config.read().splitlines()[0]
         config.close()
-        self.orangeGates = [((15, 11), (2, -2), 4),
-                            ((15, 11), (2, -2), -4),
+        self.orangeGates = [((15, 11), (2, -2), 0),
                             ((1, -1), (1, -1), -35),
-                            ((-11, -15), (2, -2), -4),
-                            ((-11, -15), (2, -2), 4),
+                            ((-11, -15), (2, -2), 0),
                             ((1, -1), (1, -1), 35)]
-        self.blueGates = [#((-11, -15), (2, -2), (5, 3)),
-            ((-11, -15), (2, -2), -4),
-                          ((-11, -15), (2, -2), 4),
+        self.blueGates = [((-11, -15), (2, -2), 0),
                           ((1, -1), (1, -1), 35),
-                          ((15, 11), (2, -2), 4),
-                          ((15, 11), (2, -2), -4),
+                          ((15, 11), (2, -2), 0),
                           ((1, -1), (1, -1), -35)]
         self.counterOrange = 0
         self.counterBlue = 0
@@ -178,10 +161,8 @@ class API:
         try:
             res = requests.get("http://"+self.IP+":6721/session", timeout=0.1)
             res = res.json()
-            #print(res)
             return res
         except :
-            #print("connection error, are you in a game?")
             return False
 
     def size(self, vector):
@@ -201,19 +182,27 @@ class API:
         :param gate:
         :return: bool
         """
-        planeNormal = np.array([0, 1, 0])
-        planePoint = np.array([gate[0][0], gate[1, 0], gate[2]])  # Any point on the plane
+        planeNormal = np.array([0, 0, 1])
+        planePoint = np.array([gate[0][0], gate[1][0], gate[2]])  # Any point on the plane
 
         # Define ray
         rayDirection = np.array([currentPos[0]-lastPos[0], currentPos[1]-lastPos[1], currentPos[2]-lastPos[2]])
         rayPoint = np.array(currentPos)  # Any point along the ray
 
-        Psi = self.LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint)
-        print("intersection at", Psi)
+        intersection = self.LinePlaneCollision(planeNormal, planePoint, rayDirection, rayPoint)
+        if intersection is not None:
+            gate_1 = [gate[0][0], gate[1][0], gate[2]]
+            gate_2 = [gate[0][1], gate[1][1], gate[2]]
+            if gate_2[0] < intersection[0] < gate_1[0] and\
+               gate_2[1] < intersection[1] < gate_1[1] and\
+               gate_2[2] == intersection[2]:
+                return True
+        else:
+            return False
 
-    def LinePlaneCollision(self, planeNormal, planePoint, rayDirection, rayPoint, epsilon=1e-6):
+    def LinePlaneCollision(self, planeNormal, planePoint, rayDirection, rayPoint, constant=1e-6):
         """
-        yes I stole this code. It comes from
+        code inspired by
         https://rosettacode.org/wiki/Find_the_intersection_of_a_line_with_a_plane#Python
         as of 15/05/2021
         :param planeNormal:
@@ -223,17 +212,16 @@ class API:
         :param epsilon:
         :return:
         """
-        ndotu = planeNormal.dot(rayDirection)
-        if abs(ndotu) < epsilon:
-            raise RuntimeError("no intersection or line is within plane")
+        normalDotProduct = planeNormal.dot(rayDirection)
+        if abs(normalDotProduct) < constant:
+            return
 
-        w = rayPoint - planePoint
-        si = -planeNormal.dot(w) / ndotu
-        Psi = w + si * rayDirection + planePoint
-        return Psi
+        pointDelta = rayPoint - planePoint
+        dotProdRatio = -planeNormal.dot(pointDelta) / normalDotProduct
+        pointOfIntersection = pointDelta + dotProdRatio * rayDirection + planePoint
+        return pointOfIntersection
 
     def checkGates(self, res=None):
-        #print(self.counterBlue)
         if not res:
             res = self.get()
         if not res:
@@ -241,28 +229,32 @@ class API:
         returnStr = ""
         blueGate = self.blueGates[self.counterBlue]
         orangeGate = self.orangeGates[self.counterOrange]
-        #print(res["teams"][0])
         bluePlayers = res["teams"][0]["players"]
-        orangePlayers = res["teams"][3]["players"]
+        orangePlayers = res["teams"][1]["players"]
         bluePosAverage = self.averagePosition(bluePlayers[0]["head"]["position"], bluePlayers[1]["head"]["position"])
         orangePosAverage = self.averagePosition(orangePlayers[0]["head"]["position"], orangePlayers[1]["head"]["position"])
-        if self.checkRay(self.bluePositions[-1], bluePosAverage, self.blueGates[self.counterBlue]):
-            returnStr += "b"
-            self.counterBlue = (self.counterBlue + 1) % len(self.blueGates)
-            #say("blue passed through gate"+str(self.counterBlue))
-            if self.counterBlue == 0:
-                self.orangeLaps += 1
-                returnStr += "B"
-        if self.checkRay(self.orangePositions[-1], orangePosAverage, self.orangeGates[self.counterOrange]):
-            self.counterOrange = (self.counterOrange + 1) % len(self.orangeGates)
-            returnStr += "o"
-            #say("orange passed through gate"+str(self.counterOrange))
-            if self.counterOrange == 0:
-                self.orangeLaps += 1
-                returnStr += "O"
+        try:
+            if self.checkRay(self.bluePositions[-1], bluePosAverage, self.blueGates[self.counterBlue]):
+                returnStr += "b"
+                self.counterBlue = (self.counterBlue + 1) % len(self.blueGates)
+                print("blue passed through gate"+str(self.counterBlue))
+                if self.counterBlue == 0:
+                    self.orangeLaps += 1
+                    returnStr += "B"
+        except IndexError:
+            pass
+        try:
+            if self.checkRay(self.orangePositions[-1], orangePosAverage, self.orangeGates[self.counterOrange]):
+                self.counterOrange = (self.counterOrange + 1) % len(self.orangeGates)
+                returnStr += "o"
+                print("orange passed 4through gate"+str(self.counterOrange))
+                if self.counterOrange == 0:
+                    self.orangeLaps += 1
+                    returnStr += "O"
+        except IndexError:
+            pass
         self.orangePositions.append(orangePosAverage)
         self.bluePositions.append(bluePosAverage)
-        #print(bluePosAverage)
         return returnStr
 
     def checkStart(self, res=None):
@@ -271,9 +263,7 @@ class API:
         if not res:
             return False, "e"
         try:
-            #print(res)
             teams = res["teams"]
-            #print(teams)
             blue = teams[0]["players"]
             orange = teams[1]
             start = False
@@ -282,13 +272,15 @@ class API:
             orange = orange["players"]
             if len(blue) < 2 or len(orange) < 2:
                 return False, "p"
-            if res["game_clock"] < 0.1:
+            if res["game_clock"] < 0.5:
                 start = True
             if res["game_status"] != "playing":
                 return start, "r"
 
             return False, "e"
-        except KeyError:
+        except KeyError as error:
+            print(error)
             return False, "l"
+
 
 appGui = GUI()
